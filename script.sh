5e63bd6760b8d51ad172778cca5f17e03a0cbfd4
Junlong Gao <junlonga@umich.edu>
2016-10-15 18:38:50 -0400

    disable code sign

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "building the project"
 xcodebuild -project Programming_Keyboard.xcodeproj -target unittests build
-xcodebuild -project Programming_Keyboard.xcodeproj -target Programming_Keyboard build
+xcodebuild -project Programming_Keyboard.xcodeproj -target Programming_Keyboard -sdk iphonesimulator10.0 'CODE_SIGN_IDENTITY=-' build
 
 echo "running unit tests"
 xcodebuild test -project Programming_Keyboard.xcodeproj -scheme unittests
