37e42985ec117d2ef325e59ef989fc8c7ef815c3
Junlong Gao <junlonga@umich.edu>
2016-11-02 00:32:55 -0400

    fix language size

--- Programming_Keyboard/CompletionLanguageSelectionController.m
+++ Programming_Keyboard/CompletionLanguageSelectionController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.preferredContentSize = CGSizeMake(190, 290);
     self.AvailableList = @[@"cpp", @"java", @"go"];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
